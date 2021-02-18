cecd04d20e431f7e952d0aa6266786e9b68a99cc
bluewow <dogseen@gmail.com>
2021-02-18 23:23:11 +0900

    - init - demo controller

---
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/demo")
+@RestController
+public class DemoController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "TEST";
+    }
+}
